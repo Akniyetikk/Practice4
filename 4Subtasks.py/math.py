fb508cee5d2e2a497a19b2b485218e377ada7f22
Akniyet <aknietnroe@gmail.com>
2026-02-28 06:54:05 +0500

    Create math.py

---
+++ 4Subtasks.py/math.py
@@ -0,0 +1,31 @@
+task1
+import math
+
+degree = float(input("Input degree: "))
+radian = math.radians(degree)
+
+print("Output radian:", radian)
+
+task2
+height = float(input("Height: "))
+base1 = float(input("Base, first value: "))
+base2 = float(input("Base, second value: "))
+
+area = (base1 + base2) / 2 * height
+print("Expected Output:", area)
+
+task3
+import math
+
+n = int(input("Input number of sides: "))
+s = float(input("Input the length of a side: "))
+
+area = (n * s**2) / (4 * math.tan(math.pi / n))
+print("The area of the polygon is:", area)
+
+task4
+base = float(input("Length of base: "))
+height = float(input("Height of parallelogram: "))
+
+area = base * height
+print("Expected Output:", area)
