f99308aeb33920aa723a062afd438ce455fd711e
Akniyet <aknietnroe@gmail.com>
2026-02-28 07:14:22 +0500

    Create JSON.py

---
+++ JSON.py
@@ -0,0 +1,282 @@
+#JSON is Javascript Object Notation
+EX1
+import json
+
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
+
+
+
+
+
+EX2
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
+
+
+
+
+
+
+EX3
+import json
+
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+
+#Result
+#{"name": "John", "age": 30}
+#["apple", "bananas"]
+#["apple", "bananas"]
+#"hello"
+#42
+#31.76
+#true
+#false
+#null
+
+
+
+
+
+
+EX4
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
+
+#Result
+#{"name": "John", "age": 30, "married": true, "divorced": false, "children": ["Ann","Billy"], "pets": null, "cars": [{"model": "BMW 230", "mpg": 27.5}, {"model": "Ford Edge", "mpg": 24.1}]}
+
+
+
+
+
+EX5
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# use four indents to make it easier to read the result:
+print(json.dumps(x, indent=4))
+
+#Result
+
+{
+    "name": "John",
+    "age": 30,
+    "married": true,
+    "divorced": false,
+    "children": [
+        "Ann",
+        "Billy"
+    ],
+    "pets": null,
+    "cars": [
+        {
+            "model": "BMW 230",
+            "mpg": 27.5
+        },
+        {
+            "model": "Ford Edge",
+            "mpg": 24.1
+        }
+    ]
+}
+
+
+
+
+
+EX6
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# use . and a space to separate objects, and a space, a = and a space to separate keys from their values:
+print(json.dumps(x, indent=4, separators=(". ", " = ")))
+
+#Result
+ 
+{
+    "name" = "John".
+    "age" = 30.
+    "married" = true.
+    "divorced" = false.
+    "children" = [
+        "Ann".
+        "Billy"
+    ].
+    "pets" = null.
+    "cars" = [
+        {
+            "model" = "BMW 230".
+            "mpg" = 27.5
+        }.
+        {
+            "model" = "Ford Edge".
+            "mpg" = 24.1
+        }
+    ]
+}
+
+
+
+
+
+EX7
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# sort the result alphabetically by keys:
+print(json.dumps(x, indent=4, sort_keys=True))
+
+#Result
+
+{
+    "age": 30,
+    "cars": [
+        {
+            "model": "BMW 230",
+            "mpg": 27.5
+        },
+        {
+            "model": "Ford Edge",
+            "mpg": 24.1
+        }
+    ],
+    "children": [
+        "Ann",
+        "Billy"
+    ],
+    "divorced": false,
+    "married": true,
+    "name": "John",
+    "pets": null
+}
+
+
+
+#deserialization
+EX8
+import json
+
+# Read from file and parse JSON
+with open("sample.json", "r") as f:
+    data = json.load(f)
+
+print(data)
+print(type(data))
+
+
+EX9
+import json
+json_str = '{"name": "Francis", "age": 25, "city": "New York"}'
+data = json.loads(json_str)
+
+print(data)
+print(type(data))
+
+
+#serialization
+
+EX10
+import json
+data = {
+    "name": "sathiyajith",
+    "rollno": 56,
+    "cgpa": 8.6,
+    "phone": "9976770500"
+}
+
+json_str = json.dumps(data, indent=4)
+with open("sample.json", "w") as f:
+    f.write(json_str)
+
+EX11 #without converting
+import json
+data = {
+    "name": "sathiyajith",
+    "rollno": 56,
+    "cgpa": 8.6,
+    "phone": "9976770500"
+}
+
+with open("sample.json", "w") as f:
+    json.dump(data, f)
